0a2475e521a7c7b3dca2f4c520d878495ae7d201
anjaliofficial <bistaanjali1415@gmail.com>
2026-07-14 09:20:35 +0545

    Equipment model

---
+++ backend/src/routes/equipment.routes.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+
+import equipmentController from "../controllers/equipment.controller";
+import { authenticate } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/role.middleware";
+const router = Router();
+
+/**
+ * Public Routes
+ */// Public
+router.get("/", equipmentController.getAllEquipment);
+
+// Protected
+router.get(
+  "/my-items",
+  authenticate,
+  authorize("owner", "admin"),
+  equipmentController.getMyEquipment
+);
+
+// Public
+router.get("/:id", equipmentController.getEquipmentById);
+
+// Protected
+router.post(
+  "/",
+  authenticate,
+  authorize("owner", "admin"),
+  equipmentController.createEquipment
+);
+
+router.put(
+  "/:id",
+  authenticate,
+  authorize("owner", "admin"),
+  equipmentController.updateEquipment
+);
+
+router.delete(
+  "/:id",
+  authenticate,
+  authorize("owner", "admin"),
+  equipmentController.deleteEquipment
+);
+
+
+export default router;
